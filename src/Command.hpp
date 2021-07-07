bd273c952e63f72bc26250e4965dde4dd8075a4e
Roout <following2future@gmail.com>
2021-07-08 00:14:46 +0300

    update command structure

--- src/Command.hpp
+++ src/Command.hpp
@@ -1,33 +1,68 @@
 #pragma once
 #include <type_traits>
+#include <vector>
+#include <string_view>
+#include <sstream>
+#include <string>
+#include <cassert>
 
 namespace command {
 
-    struct RealmID {};
-    struct RealmStatus {};
-    struct AccessToken {};
-    struct Quit {};
-
-// Type traits:
-    template<typename T>
-    struct is_blizzard_api {
-        static constexpr bool value { 
-            std::is_same_v<T, RealmID>
-            || std::is_same_v<T, RealmStatus>
-            || std::is_same_v<T, AccessToken>
-            || std::is_same_v<T, Quit>
-        };
+    struct RealmID {
+        static RealmID Create([[maybe_unused]] const std::vector<std::string_view>& params) {
+            return RealmID{};
+        }
     };
 
-    template<typename T>
-    constexpr bool is_blizzard_api_v = is_blizzard_api<T>::value;
+    struct RealmStatus {
+        static RealmStatus Create([[maybe_unused]] const std::vector<std::string_view>& params) {
+            return RealmStatus{};
+        }
+    };
+
+    struct AccessToken {
+        static AccessToken Create([[maybe_unused]] const std::vector<std::string_view>& params) {
+            return AccessToken{};
+        }
+    };
+
+    struct Shutdown {
+        
+        static Shutdown Create(const std::vector<std::string_view>& params) {
+            assert(!params.empty());
+            Shutdown cmd {};
+            for (auto it = params.cbegin(); it != params.cend(); ++it) {
+                cmd.message_ += std::string(*it) +  " ";
+            }
+            return cmd;
+        }
+
+    private:
+        std::string message_;
+    };
+
+    namespace details {
+    // Type traits:
+        template<typename T>
+        struct is_blizzard_api {
+            static constexpr bool value { 
+                std::is_same_v<T, RealmID>
+                || std::is_same_v<T, RealmStatus>
+                || std::is_same_v<T, AccessToken>
+                || std::is_same_v<T, Quit>
+            };
+        };
+
+        template<typename T>
+        constexpr bool is_blizzard_api_v = is_blizzard_api<T>::value;
+    }
 
 // Helper functions:
     template<typename Command, typename Executor,
-        typename = std::enable_if_t<is_blizzard_api_v<Command>>
+        typename = std::enable_if_t<details::is_blizzard_api_v<Command>>
     >
-    auto Execute(Command&& cmd, Executor& ex) {
-        return ex.Execute(std::forward<Command>(cmd));
+    void Execute(Command&& cmd, Executor& ex) {
+        ex.Execute(std::forward<Command>(cmd));
     }
 
 }
