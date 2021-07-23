8986757afe9304856f4bd1ab081100cc3dd58b21
Roout <following2future@gmail.com>
2021-07-23 14:14:05 +0300

    log outgoing irc messages

--- src/Connection.cpp
+++ src/Connection.cpp
@@ -381,6 +381,7 @@ void IrcConnection::OnRead(const boost::system::error_code& error, size_t bytes)
             boost::asio::buffers_begin(data) + bytes - kCRLF.size()
         };        
         inbox_.consume(bytes);
+        log_->Write(LogType::info, "->", buffer, '\n');
         message_ = net::irc::ParseMessage(std::string_view { buffer.data(), buffer.size() } );
 
         if (onReadSuccess_) {
