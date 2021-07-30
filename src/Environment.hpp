5a128c0b501d7d74533a19c78cb685d5135c3993
Roout <following2future@gmail.com>
2021-07-30 17:18:58 +0300

    add coupling of service names and enum value

--- src/Environment.hpp
+++ src/Environment.hpp
@@ -1,8 +1,51 @@
 #pragma once
 #include <cstddef>
+#include <type_traits>
+#include <optional>
+#include <string_view>
+#include <array>
+
+namespace service {
+
+    // coupling enum value with string_view in both ways
+    class Couple final {
+    public:
+
+        enum ServiceKind {
+            kBlizzard,
+            kTwitch,
+            kConsole,
+            kCount
+        };
+
+        constexpr Couple() 
+            : services_ { "blizzard", "service", "twitch" }
+        {}
+
+        constexpr std::string_view Get(ServiceKind kind) const noexcept {
+            return services_[kind];
+        }
+
+        constexpr std::optional<ServiceKind> Get(
+                std::string_view cmd) const noexcept 
+        {
+            for (size_t i = 0; i < kCount; i++) {
+                if (services_[i] == cmd) {
+                    return { static_cast<ServiceKind>(i) };
+                }
+            }
+            return std::nullopt;
+        }
+
+    private:
+        const std::array<std::string_view, kCount> services_;
+    };
+
+} // namespace service
 
 namespace cst {
 
 constexpr std::size_t kQueueCapacity { 255 };
+inline constexpr service::Couple kServiceCouple {};
 
 } // namespace cst
