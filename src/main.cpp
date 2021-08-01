b7c7e86b4e785f6b789a6c82716cd15ddffb4f88
Roout <following2future@gmail.com>
2021-08-01 14:04:17 +0300

    fix console output for utf8 charset

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "App.hpp"
-
+#include <locale>
 int main() {
+    // for work with console
+    std::locale::global(std::locale("en_US.UTF-8"));
     App app;
     app.Run();
     return 0;
