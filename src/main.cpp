8717d56285b2cc726af0000212a822b4898ee93c
Roout <following2future@gmail.com>
2021-08-01 15:23:18 +0300

    remove thousands separator for numeric values output

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "App.hpp"
 #include <locale>
+
 int main() {
     // for work with console
-    std::locale::global(std::locale("en_US.UTF-8"));
+    std::locale locale { std::locale::classic(), "en_US.UTF-8", std::locale::ctype };
+    std::locale::global(locale);
     App app;
     app.Run();
     return 0;
