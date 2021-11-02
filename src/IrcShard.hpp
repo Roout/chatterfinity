c8ff20a20781f35eb137db6c3ae4a5af11122942
Roout <following2future@gmail.com>
2021-11-02 15:12:32 +0300

    following changes introduced: * update Ticket interface; * switched to usage of shared_ptr with tickets; * synchronize access to message buckets; * add more detailed commets concering thread-safety

--- src/IrcShard.hpp
+++ src/IrcShard.hpp
@@ -8,8 +8,8 @@
 #include <string>
 #include <vector>
 #include <array>
-#include <unordered_map>
 #include <memory>
+#include <mutex>
 
 #include "Command.hpp"
 #include "Translator.hpp"
@@ -23,30 +23,83 @@ namespace service::twitch {
 using TimePoint = std::chrono::time_point<std::chrono::steady_clock>;
 using Seconds = std::chrono::seconds;
 
+/**
+ * Usage:
+ * 1. Use bucket to acquire ticket for irc connection for 
+ * command execution (if bucket is not empty)
+ * 2. Ticket must be passed to `onWrite` callback in execution chain 
+ * and explicitly called Ticket::Release there.
+ * 
+ * Note: only MessageBucket::refillAmount_ tickets can exist at once.
+ * It makes impossible to use `refillAmount_` tickets right before refilling
+ * and `refillAmount_` right after(i.e., legaly use 2X tickets in short 
+ * amount of time <= `refillRate_`) in short timelapse, 
+ * however it prevents usage more than `refillAmount_` tickets at once.
+ *
+ * As answering to EVERY request of the users deemed not very important
+ * I don't enqueue requests which can't get ticket due to rate-limit
+ * I just abandon them.
+ *
+ * TODO: maybe queue message which will notify that some message were ignored
+ * and advice to repeat it again
+ */
 class MessageBucket final {
 public:
+    class Ticket final {
+    public:
+
+        Ticket(MessageBucket* bucket);
+        
+        void Release() noexcept;
+
+    private:
+        // Assume, bucket can't be destroyed 
+        // while IrcShard(and IrcConnection) exists
+        MessageBucket* const bucket_ { nullptr };
+        bool released_ { false };
+    };
+
     MessageBucket(std::uint16_t amount, Seconds rate);
 
-    // Actually, it's not precision approach 
-    // cuz we lose some tickets(opportunity to write)
-    // but I can avoid using timer
-    bool TryUse();
+    // Q: Why Not Optional?
+    // A: Used shared_ptr instead of optional with move semantic 
+    // because capturing move-only object will make move-only lambda 
+    // so I won't be able to create callback of type `std::function<void()>`
+    // which has requirement Copy[Assignable|Constructable]
+    std::shared_ptr<Ticket> TryAcquire();
+
+    void Release();
 
 private:
+    
+    void TryRefill();
+
     // `bucket size`
     // differ for user mode and moderator mode
     const std::uint16_t refillAmount_;
     // amount of available messages for IRC connection
     std::uint16_t available_;
+    // already consumed during the last refill
+    std::uint16_t consumed_;
     // shows how much time needs to pass to fill the bucket
     // Note: differ for user mode and moderator mode
     const Seconds refillRate_;
     TimePoint lastRefillTime_;
+
+    mutable std::mutex mutex_;
 };
 
-// namespace ssl = boost::asio::ssl;
-// using boost::asio::ip::tcp;
-
+/**
+ * Thread-safety concerns:
+ *  IrcShard is being touched (after being constructed) 
+ *  only by `Twitch::ResetWork` and `Twitch::Execute`
+ *  so these are narrow places.
+ * 
+ * 1. service::Twitch's destructor returns after all work which is being posted to `io_context`
+ *  is done.
+ * 2. After completion of all work in `io_context` the `IrcConnection` will exist only in 
+ * `IrcShard` (ONLY 1 strong reference). See `assert` in `~IrcShard()`
+ */
 class IrcShard final {
 public:
     using SharedIO = std::shared_ptr<boost::asio::io_context>;
@@ -58,6 +111,11 @@ public:
         , SharedIO context
         , SharedSSL ssl);
 
+    // ASSUME: `irc_->ScheduleShutdown()` has already been called, 
+    // i.e., IrcShard::Reset() had been already invoked before the ~IrcShard
+    // 
+    // TODO: Reset may no be called if excption is thrown. 
+    //       Find and resolve such dangerous places.
     ~IrcShard();
 
     void Reset();
@@ -74,7 +132,7 @@ private:
     void HandlePrivateMessage(net::irc::Message& message);
 
 private:
-    // IRC limits: https://dev.twitch.tv/docs/irc/guide
+    // Twitch's IRC limits: https://dev.twitch.tv/docs/irc/guide
     static constexpr std::uint16_t kRefillAmount{ 20 };
     static constexpr Seconds kGeneralRefillRate{ 10 };
     static constexpr Seconds kChannelRefillRate{ 30 };
@@ -84,27 +142,34 @@ private:
     };
 
     enum Bucket: uint16_t { 
-        kGeneral, // auth + join
-        kChannel, // privmsg + cap [sending commands or messages to channels]
+        kGeneral, // PASS, JOIN
+        kChannel, // PRIVMSG
         kCount
     };
-    const std::array<MessageBucket, Bucket::kCount> buckets_;
-    std::unordered_map<std::string_view, Bucket> bucketByCommand_;
+    std::array<MessageBucket, Bucket::kCount> buckets_;
     
-    // stuff from the twitch context 
+    // Stuff from the twitch context;
     // required for creating HTTP connection
     Twitch *const service_ { nullptr };
     command::Queue *const commands_ { nullptr };
     Translator translator_;
-    // keep bindings of [aliases] to [commands with parameters]
+    // Keep bindings of [aliases] to [commands with parameters]
     command::AliasTable *const aliases_ { nullptr };
 
-    // connection
+    // Contexts for connection's creation
     SharedIO context_;
     SharedSSL ssl_;
+
+    // Thread-safety:
+    // - `shared_ptr` - used only with const methods so no data race here.
+    //   As long as `irc_` is not reseted manually no data race will occur.
+    // - `IrcConnection` object - it cannot live more than `IrcShard` because 
+    //  the only thing that can prolong it's life is `context_` 
+    //  whereas `context_` should complete all work by the time the IrcShard 
+    //  will be destroyed.
     std::shared_ptr<IrcConnection> irc_;
 
-    // list of connected channels
+    // List of connected channels
     std::vector<Channel> channels_;
 
     class Invoker;
@@ -116,11 +181,11 @@ public:
     Invoker(IrcShard *shard);
 
     void Execute(command::Help);
+    void Execute(command::Ping);
+    void Execute(command::Pong);
     void Execute(command::Shutdown);
     void Execute(command::Validate);
     void Execute(command::Login);
-    void Execute(command::Ping);
-    void Execute(command::Pong);
     void Execute(command::Join);
     void Execute(command::Leave);
     void Execute(command::Chat);
